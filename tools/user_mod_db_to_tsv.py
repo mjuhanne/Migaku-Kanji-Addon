2dd6589df8cac0e479fa1612a4a7cb0b0f5f8843
mjuhanne <marko.juhanne@gmail.com>
2023-11-27 22:43:07 +0200

    Update the script for extracting user modified values to TSV file to reflect new Story DB

--- tools/user_mod_db_to_tsv.py
+++ tools/user_mod_db_to_tsv.py
@@ -7,81 +7,29 @@ import re
 # A tool to extract user modified fields and output them to a .tsv patch file
 #
 
-user_modifiable_fields = ['primitives','sec_primitives','primitive_keywords','heisig_story','heisig_comment']
+user_modifiable_fields = ['primitives','keywords','primitive_keywords','story','comment']
 
-# Creates a list of single-character Unicode kanjis and [primitive] tags
-# For example '[banner]也' -> ['\[banner\]','也'] 
-def custom_list(l):
-    if l is None:
+def j2c_or_none(d):
+    if d is None or d == '':
         return None
-    g = re.findall(r'([^\[]|\[[^\]]+\])',l)
-    return g
-
-def json_loads_or_none(l):
-    if l is None:
-        return None
-    return json.loads(l)
-
-def list_to_primitive_str(l):
-    return ''.join(l)
-
-def to_json_list_str(csv):
-    if csv!= '':
-        item_list = csv.split(',')
-        clean_list = [item.strip() for item in item_list]
-        return json.dumps(clean_list)
-    return '[]'
-
-def j2c(d):
     return ", ".join(json.loads(d))
 
+def story_j2c(d):
+    if d is None or d == '' or d == '[]':
+        return ''
+    d = json.loads(d)
+    if len(d)==1:
+        s = d[0]
+        if '\n' not in s and '\r' not in s:
+            return s
+    return json.dumps(d)
 
-# (field_name, load_function, column)
-_ = lambda x: x
-requested_fields = [
-    ("character", _, "characters.character"),
-    ("stroke_count", _, None),
-    ("onyomi", json.loads, None),
-    ("kunyomi", json.loads, None),
-    ("nanori", json.loads, None),
-    ("meanings", json.loads, None),
-    ("frequency_rank", _, None),
-    ("grade", _, None),
-    ("jlpt", _, None),
-    ("kanken", _, None),
-    ("primitives", custom_list, None),
-    ("sec_primitives", custom_list, None),
-    ("primitive_of", custom_list, None),
-    ("primitive_keywords", json.loads, None),
-    ("primitive_alternatives", custom_list, None),
-    ("heisig_id5", _, None),
-    ("heisig_id6", _, None),
-    ("heisig_keyword5", _, None),
-    ("heisig_keyword6", _, None),
-    ("heisig_story", _, None),
-    ("heisig_comment", _, None),
-    ("radicals", list, None),
-    ("words_default", json.loads, None),
-    ("koohi_stories", json.loads, None),
-    ("wk", _, None),
-    ("usr_keyword", _, "usr.keywords.usr_keyword"),
-    ("usr_primitive_keyword", _, "usr.keywords.usr_primitive_keyword"),
-    ("usr_story", _, "usr.stories.usr_story"),
-    ("mod_heisig_story", _, "usr.modified_values.mod_heisig_story"),
-    ("mod_heisig_comment", _, "usr.modified_values.mod_heisig_comment"),
-    ("mod_primitives", custom_list, "usr.modified_values.mod_primitives"),
-    ("mod_sec_primitives", custom_list, "usr.modified_values.mod_sec_primitives"),
-    ("mod_primitive_keywords", json_loads_or_none, "usr.modified_values.mod_primitive_keywords"),
-]
 
 convert_data_from_db_to_str_func = {
-    "onyomi"                    : j2c,
-    "kunyomi"                   : j2c,
-    "nanori"                    : j2c,
-    "meanings"                  : j2c,
-    "primitive_keywords"        : j2c,
-    "words_default"             : j2c,
-    "koohi_stories"             : j2c,
+    "keywords"                : j2c_or_none,
+    "primitive_keywords"      : j2c_or_none,
+    "story"                   : story_j2c,
+    "comment"                 : story_j2c,
 }
 
 def data_from_db_to_str(field_name,data):
@@ -90,28 +38,24 @@ def data_from_db_to_str(field_name,data):
         return f(data)
     return data
 
-kanji_db_path = sys.argv[1] if len(sys.argv) > 1 else "addon/kanji.db"
-user_db_path = sys.argv[2] if len(sys.argv) > 2 else "addon/user_files/user.db"
+story_db_path = sys.argv[1] if len(sys.argv) > 1 else "addon/story.db"
+user_mod_db_path = sys.argv[2] if len(sys.argv) > 2 else "addon/user_files/usermod.db"
 tsv_path = sys.argv[3] if len(sys.argv) > 3 else "kanji-usermod-patch.tsv"
 
-con = sqlite3.connect(kanji_db_path)
-crs = con.cursor()
+story_con = sqlite3.connect(story_db_path)
+story_crs = story_con.cursor()
 
-crs.execute(f'ATTACH DATABASE "{user_db_path}" AS usr;')
+user_con = sqlite3.connect(user_mod_db_path)
+user_crs = user_con.cursor()
 
-fields = ",".join((rf[2] if rf[2] else rf[0]) for rf in requested_fields)
 
-joins = [
-    f"LEFT OUTER JOIN usr.keywords ON characters.character == usr.keywords.character ",
-    f"LEFT OUTER JOIN usr.stories ON characters.character == usr.stories.character ",
-    f"LEFT OUTER JOIN usr.modified_values ON characters.character == usr.modified_values.character "
-]
-joins_txt = "".join(joins)
+fields = ['source','character'] + ['mod_' + field for field in user_modifiable_fields]
+fields_str = ','.join(fields)
 
-crs.execute(
-    f"SELECT {fields} FROM characters {joins_txt}"
+user_crs.execute(
+    f"SELECT {fields_str} FROM modified_values"
 )
-rows = crs.fetchall()
+mod_rows = user_crs.fetchall()
 
 
 f = open(tsv_path, "w", encoding="utf-8")
@@ -126,25 +70,44 @@ def fw(args):
 
 # write header
 fw( [
-    "Kanji",
+    "Source",
+    "Character",
     "Field",
     "OldValue",
     "NewValue",
 ]
 )
 
-for raw_data in rows:
+orig_fields = [field for field in user_modifiable_fields]
+orig_fields_str = ','.join(orig_fields)
 
-    ret = {}
+for raw_data in mod_rows:
 
-    for data, (name, load_func, _) in zip(raw_data, requested_fields):
-        ret[name] = data #load_func(data)
+    new_data = {}
 
-    character = ret['character']
+    for data, field in zip(raw_data, fields):
+        new_data[field] = data #load_func(data)
+
+    character = new_data['character']
+    source = new_data['source']
+
+    story_crs.execute(
+        f"SELECT {orig_fields_str} FROM stories WHERE character == ? AND source == ?",
+        (character,source)
+    )
+    orig_rows = story_crs.fetchall()
+    orig_data = {}
+    if len(orig_rows) == 1:
+        for data, field in zip(orig_rows[0], orig_fields):
+            orig_data[field] = data #load_func(data)
 
     for field in user_modifiable_fields:
-        orig_data = data_from_db_to_str(field,ret[field]) or ''
-        user_mod_data = data_from_db_to_str(field,ret['mod_' + field])
+        if field in orig_data:
+            data = data_from_db_to_str(field,orig_data[field]) or ''
+        else:
+            data = ''
+            
+        user_mod_data = data_from_db_to_str(field,new_data['mod_' + field])
 
         if user_mod_data is not None:
             user_mod_data = user_mod_data.replace('\n','')
@@ -153,7 +116,7 @@ for raw_data in rows:
             if orig_data == user_mod_data:
                 print("%s: user modified field %s has value %s which equals original. Should be cleaned!" % (character, field, orig_data))
             else:
-                change = [character,field,orig_data,user_mod_data]
+                change = [source,character,field,data,user_mod_data]
                 fw(change)
 
 f.close()
